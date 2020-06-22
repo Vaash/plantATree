002fc6037ced99b7ff951a6783bb738696716e16
Bastien Delboé <bastiendelboe@mbp-de-bastien.home>
2020-06-22 17:59:23 +0200

    : Leaflet map display

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 import '../css/app.scss';
 
-let myMap = L.map('mapId').setView([47.219568, 2.588194], 13);
+let myMap = L.map('mapId').setView([47.219568, 2.588194], 6);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -8,5 +8,5 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
+    accessToken: 'pk.eyJ1IjoidmFhc2giLCJhIjoiY2ticW9saGxmMXFsczJxcG9pdm1tdHhhaCJ9.FY98gEYe63iJUubsFtv9dw'
 }).addTo(myMap);
